fa7fce8aba1ed4228819029356cf88659fd528c8
Caesarovich <cesar@caesarovich.xyz>
2024-12-10 01:12:55 +0100

    Change vercel analytics import

--- src/app.tsx
+++ src/app.tsx
@@ -3,7 +3,6 @@ import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import "./app.css";
-import { Analytics } from "@vercel/analytics/react";
 import Nav from "./components/Nav";
 
 export default function App() {
@@ -17,7 +16,6 @@ export default function App() {
 
 					<Nav />
 					<Suspense>{props.children}</Suspense>
-					<Analytics />
 				</>
 			)}
 		>
--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,5 +1,8 @@
 // @refresh reload
 import { StartClient, mount } from "@solidjs/start/client";
+import { inject } from '@vercel/analytics';
+
+inject();
 
 // biome-ignore lint/style/noNonNullAssertion: Default behavior
 mount(() => <StartClient />, document.getElementById("app")!);
