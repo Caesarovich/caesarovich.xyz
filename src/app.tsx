6a821329d20560a84c11fa607e6fac0919aacbf7
Caesarovich <cesar@caesarovich.xyz>
2024-12-10 01:07:48 +0100

    Add Vercel analytics

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,7 @@ import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import "./app.css";
+import { Analytics } from "@vercel/analytics/react";
 import Nav from "./components/Nav";
 
 export default function App() {
@@ -16,6 +17,7 @@ export default function App() {
 
 					<Nav />
 					<Suspense>{props.children}</Suspense>
+					<Analytics />
 				</>
 			)}
 		>
